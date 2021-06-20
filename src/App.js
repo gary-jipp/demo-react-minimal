67e33dd017a2a3a51f15da3e77139730e115e50d
Gary J <github@accu.ca>
2021-06-20 11:54:57 -0700

    Add App as file

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+export default function App() {
+  
+  return (
+    <div className="App">
+      Hello React App
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "App";
 
 // Webpack won't be able to detect changes to App if in index.js
-// Move App to its own App.js file for real development
-function App() {
-
-  return (
-    <div>
-      Hello World
-    </div>
-  );
-}
+// App Needs to be its own App.js file for real development
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
