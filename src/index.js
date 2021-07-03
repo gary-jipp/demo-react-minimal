feb3b5ea9d4115c553184801164e52e58bb6fab9
Gary J <github@accu.ca>
2021-07-02 19:27:09 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import App from "App";
 // Webpack won't be able to detect changes to App if in index.js
 // App Needs to be its own App.js file for real development
 
-ReactDOM.render(<App />, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
